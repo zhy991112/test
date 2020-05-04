22416bc9d90f2d5a22bb3761c82412c653c704aa
勇 祝 <zhy991112@hotmail.com>
2020-05-04 14:53:09 +0800

    zhy1212

--- MFCApplication1/MFCApplication1Dlg.cpp
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -100,8 +100,7 @@ BOOL CMFCApplication1Dlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
-	LPMESSAGEFILTER;
-	this is curretn;
+
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
